9420f043e71f3b26b9baa4e980ebae4954c89027
Ashish Singh <ashish.singh@niceactimize.com>
2020-04-21 11:47:41 +0530

    Updates

---
+++ precache-manifest.985fc7f82f4048a4a103ed3facaa012c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8015ced8c89d60988b25a499e2516d4c",
+    "url": "/react-taskmanager/index.html"
+  },
+  {
+    "revision": "6b2e5e8ee424400a754e",
+    "url": "/react-taskmanager/static/css/main.2d1fe463.chunk.css"
+  },
+  {
+    "revision": "45019d51bcd2488d253f",
+    "url": "/react-taskmanager/static/js/2.d380d686.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-taskmanager/static/js/2.d380d686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b2e5e8ee424400a754e",
+    "url": "/react-taskmanager/static/js/main.20e2d337.chunk.js"
+  },
+  {
+    "revision": "96553b7b2059baa3e936",
+    "url": "/react-taskmanager/static/js/runtime-main.a224effe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-taskmanager/static/media/logo.5d5d9eef.svg"
+  }
+]);
